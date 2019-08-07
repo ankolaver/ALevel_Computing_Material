9c310e0f1b104fffe07b189a80b3648606a1cd6f
Kennethkcpdhs <47784720+Kennethkcpdhs@users.noreply.github.com>
2019-08-07 13:00:38 +0800

    Create binsearecursivelistindex.py

---
+++ Algorithms/Search/binsearecursivelistindex.py
@@ -0,0 +1,23 @@
+def binarysearch(arr, target):
+    mid = len(arr)//2
+
+    if target==arr[mid]:
+        print(arr[mid])
+
+    elif len(arr)==1:
+        print("Cannot find")
+        return False
+    
+    elif target>arr[mid]:
+        return binarysearch(arr[mid:], target)
+
+    elif target<arr[mid]:
+        return binarysearch(arr[:mid], target)
+
+    return arr
+    
+array1 = [1,2,3,4,5,6,7,8,9,10]
+
+binarysearch(array1,12)
+
+binarysearch(array1,3)
