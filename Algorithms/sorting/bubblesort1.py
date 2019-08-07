3f33793beacc49c3e90b367292cb9a900ddf180d
Kennethkcpdhs <47784720+Kennethkcpdhs@users.noreply.github.com>
2019-08-07 21:59:42 +0800

    Update bubblesort1.py

--- Algorithms/sorting/bubblesort1.py
+++ Algorithms/sorting/bubblesort1.py
@@ -1,3 +1,10 @@
+'''Bubble sort has a worst-case and average complexity
+of О(n2), where n is the number of items being sorted.
+Most practical sorting algorithms have substantially
+better worst-case or average complexity, often O(n log n).
+The function below always runs O(n^2) time even if the array is sorted.
+It can be optimized by stopping the algorithm if inner loop didn’t cause any swap.'''
+
 def bubblesort(arr):
     lenarr = len(arr)
 
@@ -7,13 +14,11 @@ def bubblesort(arr):
             if arr[k]>arr[k+1]:
                 arr[k],arr[k+1] = arr[k+1], arr[k]
                 swapmulti = True
+                #do not need to go through loop again (efficient)
 
         if swapmulti == False:
             break
 
-        toprint = []
-        for i in range(len(arr1)):
-            toprint.append(i)
     print(arr1)
 
 arr1 = [12,2,34,29,4,6,6,7]
