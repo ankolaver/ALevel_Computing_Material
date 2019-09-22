7d8b3e98cb763f28f9ba4b93c4421e034484b33d
Kennethkcpdhs <47784720+Kennethkcpdhs@users.noreply.github.com>
2019-09-22 18:22:17 +0800

    Update queuelinked1.py

--- Algorithms/datastructures/queuelinked1.py
+++ Algorithms/datastructures/queuelinked1.py
@@ -17,12 +17,11 @@ class Queue:
         pass # queues cannot become full
     
     #add data in from the back
-    def enqueue(self, data):
-        #creation of node and link
-        node = Node(data)
-        #ensure that the front is completely filled up before going to the back
+    def enqueue(self, inputdata):
+        node = Node(inputdata)
+        #ensure that the "Arbitary' front is completely filled up before going to the back
         if self.is_empty():
-            self.front = node
+            self.front = node 
         else:
             self.rear.link = node
         self.rear = node
@@ -33,7 +32,7 @@ class Queue:
             print("Cannot delete from the empty queue")
             return -1 #exit dequeue function immediately
         else:
-            removedata = self.front.data = None #data to remove
+            removedata = self.front.data = None 
             if self.front.link == None: #last node
                 self.front = self.rear = None 
                 
@@ -47,7 +46,7 @@ class Queue:
         else:
             curr = self.front
             while curr != None:
-                print(curr.data, end = ' ')#separator
+                print(curr.data, end = '  ')
                 curr = curr.link
         print("\ncurr length:",self.length)
 
@@ -60,4 +59,5 @@ q.enqueue("Mi-A3")
 q.dequeue()
 q.dequeue()
 q.display()  
+    
     
