3974447971d03b0b7808498fbf01c19403288b91
Kennethkcpdhs <47784720+Kennethkcpdhs@users.noreply.github.com>
2019-09-22 16:29:53 +0800

    Update stacklinkedlist1.py

--- Algorithms/datastructures/stacklinkedlist1.py
+++ Algorithms/datastructures/stacklinkedlist1.py
@@ -1,44 +1,43 @@
 class Node:
     def __init__(self, data):
-		self.data = data
-		self.prev = None
+        self.data = data
+        self.prev = None
         #previous data
 
 class stack:
-	def __init__(self):
-		self.top = None
-	
-	def empty(self):
-		return self.top==None
-	
-	def full(self):
-		#not possible
-		pass
-	
-	def push(self, inputdata):
-		if self.full(): #not possible
-			return -1
-		else:
-			newnode = Node(inputdata)
+    def __init__(self):
+        self.top = None
+
+    def empty(self):
+        return self.top==None
+
+    def full(self):
+        #not possible
+    pass
+
+    def push(self, inputdata):
+        if self.full(): #not possible
+            return -1
+        else:
+            newnode = Node(inputdata)
             #adding previous item to the next connection
-			newnode.prev = self.top
-			self.top = newnode
-		
-	def pop(self):
-		if self.empty():
-			print("stack empty alrdy")
-			return -1
-		else:
-			deletedata = self.top
-			self.top = self.top.prev
-			deletedata = None
-	
-	def display(self):
-		curr = self.top
-		
-		while curr is not None:
-			print(curr.data, end=' ')
-			curr = curr.prev
+            newnode.prev = self.top
+            self.top = newnode
+
+    def pop(self):
+        if self.empty():
+            print("stack empty alrdy")
+            return -1
+        else:
+            deletedata = self.top
+            self.top = self.top.prev
+            deletedata = None
+
+    def display(self):
+        curr = self.top
+        while curr is not None:
+            print(curr.data, end=' ')
+            curr = curr.prev
 		
 #main	
 s = stack()
