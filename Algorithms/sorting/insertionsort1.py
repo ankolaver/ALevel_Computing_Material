573138bb886f19cc607e84916d41c4bd61105119
Kennethkcpdhs <47784720+Kennethkcpdhs@users.noreply.github.com>
2019-09-23 13:43:25 +0800

    Update insertionsort1.py

--- Algorithms/sorting/insertionsort1.py
+++ Algorithms/sorting/insertionsort1.py
@@ -9,3 +9,23 @@ def insertion(arr):
 
 array = [3,9,12,1,2,4]
 insertion(array)
+
+
+#correct copy
+def insertionSort(arr): 
+  
+    # Traverse through 1 to len(arr) 
+    for i in range(1, len(arr)): 
+        key = arr[i] 
+        j = i-1
+        while j >= 0 and key < arr[j] : 
+                arr[j + 1] = arr[j] 
+                j -= 1
+        arr[j + 1] = key 
+  
+  
+#main
+arr = [12, 11, 13, 5, 6] 
+insertionSort(arr) 
+for i in range(len(arr)): 
+    print ("% d" % arr[i]) 
