bf0f149532ddd151473c324defa0ac6ac9d8402f
Kennethkcpdhs <47784720+Kennethkcpdhs@users.noreply.github.com>
2020-01-07 21:42:29 +0800

    Update app.py

--- flask1/app.py
+++ flask1/app.py
@@ -5,11 +5,13 @@ app = Flask(__name__)#turn flask app in to a web app
 @app.route("/")
 def index(): #name it anything
     #return "hello, world" #<simply prints out hello world>
-    name = request.args.get("name","world. It a deep deep place.Stay zen") #second value is in case user does not provide one
+    #name = request.args.get("name","world. It a deep deep place.Stay zen") #second value is in case user does not provide one
     #if not name:
         #name = <second method to do stuff>
-    return render_template("index.html", personal=name) #this is a variable
+    #return render_template("index.html", personal=name) #this is a variable
+    return render_template("index.html")
 
+'''
 @app.route("/register", methods=["POST"])
 def register():
     name = request.form.get("name")
@@ -17,3 +19,7 @@ def register():
     if not name or not dorm:
         return "Failure! Oof try again?"
     return render_template("success.html")
+'''
+
+if __name__ == "__main__":
+    app.run(host="localhost", port=5000, debug=True)
