285249277eb00a4ca64850497ffe3bd1bfc77fb4
Kennethkcpdhs <47784720+Kennethkcpdhs@users.noreply.github.com>
2020-03-10 21:39:55 +0800

    Update bstsample.py

--- Algorithms/bstsample.py
+++ Algorithms/bstsample.py
@@ -95,6 +95,10 @@ class BST:
         else:
             return self.right.maximum() 
             
+    
+    #(a) Inorder (Left, Root, Right) : 4 2 5 1 3
+    #(b) Preorder (Root, Left, Right) : 1 2 4 5 3
+    #(c) Postorder (Left, Right, Root) : 4 5 2 3 1
     def inorder(self):
         if self.left: # if self.left is not None:
             self.left.inorder()
