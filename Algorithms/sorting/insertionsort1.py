8b879cc53db819e629aa8a3e64bde16e0b1db066
Kennethkcpdhs <47784720+Kennethkcpdhs@users.noreply.github.com>
2020-03-11 12:26:55 +0800

    Update insertionsort1.py

--- Algorithms/sorting/insertionsort1.py
+++ Algorithms/sorting/insertionsort1.py
@@ -1,16 +1,3 @@
-def insertion(arr):
-    
-    for k in range(1,len(arr)):
-        temp = k
-        while temp>0 and arr[temp-1]>arr[temp]:
-            arr[temp-1],arr[temp] = arr[temp],arr[temp-1]
-            temp-=1
-    print(arr)
-
-array = [3,9,12,1,2,4]
-insertion(array)
-
-
 #correct copy
 def insertionSort(arr): 
   
