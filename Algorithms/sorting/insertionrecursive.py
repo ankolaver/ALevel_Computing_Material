50d6cdb830ea791ea58fe6236205f73ee525150b
Kennethkcpdhs <47784720+Kennethkcpdhs@users.noreply.github.com>
2020-06-12 19:09:45 +0800

    Update insertionrecursive.py

--- Algorithms/sorting/insertionrecursive.py
+++ Algorithms/sorting/insertionrecursive.py
@@ -1,4 +1,5 @@
 def insertionsort(arr,i):
+    #base case, stop iterating once it reaches end of arr
     if i>0 and i<len(arr):
         key = arr[i]
         temp = i-1
@@ -6,5 +7,8 @@ def insertionsort(arr,i):
             arr[temp+1] = arr[temp]
             temp -= 1 
         arr[temp+1] = key
-        print(arr)
+        #print(arr)
         return insertionsort(arr,i+1)
+
+item = [5,10,3,1,2,7,7,4]
+insertionsort(item,1)
